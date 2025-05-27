9cc5d1a2443fafca5da9325f89f4e3e533d9bc46
Mask-LR <2080204533@qq.com>
2025-05-27 16:36:58 +0800

    v0.0.1

--- internal/handler.go
+++ internal/handler.go
@@ -3,10 +3,10 @@ package internal
 import (
     "fmt"
     "net/http"
-    "github.com/MaskLR/MaskLR-Go/internal"
+    "github.com/MaskLR/MaskLR-Go/db"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-    msg := internal.TestDB()
+    msg := db.TestDB()
     fmt.Fprintf(w, "你好！数据库连接测试结果：%s\n", msg)
 }
