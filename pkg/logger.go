7924c52c41f31fd96d3be93ea443b725d768b471
Mask-LR <2080204533@qq.com>
2025-05-29 16:00:36 +0800

    Update logger.go
    优化时间获取

--- pkg/logger.go
+++ pkg/logger.go
@@ -3,9 +3,16 @@ package pkg
 import (
 	"log"
 	"os"
+	"time"
 )
 
 func InitLogger() {
+	// 强制设置本地时区为操作系统时区
+	loc, err := time.LoadLocation("Local")
+	if err == nil {
+		time.Local = loc
+	}
+
 	logFile, err := os.OpenFile("masklr.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Println("无法创建日志文件，使用默认输出")
