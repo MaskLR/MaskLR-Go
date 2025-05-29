f9c2e514f06e3dbf932bf15fa01cb5df8fd18085
Mask-LR <2080204533@qq.com>
2025-05-29 21:38:15 +0800

    v0.0.4
    2025/5/29

--- pkg/logger.go
+++ pkg/logger.go
@@ -3,21 +3,14 @@ package pkg
 import (
 	"log"
 	"os"
-	"time"
 )
 
 func InitLogger() {
-	// 强制东八区
-	_ = os.Setenv("TZ", "Asia/Shanghai")
-	loc, err := time.LoadLocation("Local")
-	if err == nil {
-		time.Local = loc
-	}
-
 	logFile, err := os.OpenFile("masklr.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Println("无法创建日志文件，使用默认输出")
 		return
 	}
 	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags)
 }
